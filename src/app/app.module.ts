2497f3d5880f95799f341acf2843eac0f15aa6b6
sushilsree98 <sushil.sree98@gmail.com>
2020-02-12 08:57:10 +0530

    Initial files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyListComponent } from './body-list/body-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostComponent } from './post/post.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyListComponent,
+    FooterComponent,
+    PostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
